dce42523689b9eb0f62c577168eec9ce002edfde
Makky Kaliser <49575758+makkykaliser@users.noreply.github.com>
2024-02-19 18:58:54 -0500

    no more console errors?

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
-console.debug('intializing...')
-init()
-
 const Tile = { // Tile state enumeration
     blank: "",
     x: "x",
     o: "o"
 }
 
-var board = [...Array(3)].map(e => Array(3)) // This makes an empty 3 x 3 2D array
+var board = initializeBoard(3)
+
+console.debug('intializing...')
+init()
 
 function init() {
     console.debug(board)
     refreshBoard(board)
 }
 
+function initializeBoard(size) {
+    var boardResult = []
+    for (var i = 0; i < size; i++) {
+        boardResult[i] = []
+        for (var j = 0; j < size; j++) {
+            boardResult[i][j] = Tile.blank
+        }
+    }
+    return boardResult
+}
+
 function refreshBoard(board) {
     var currentRowIndex = 0
     board.forEach( (row) => {
-        const rowElement = document.createElement('div').setAttribute('id', `row-${currentRowIndex}`)
-        document.getElementById('tic-tac-toe-board').appendChild(rowElement)
+        const rowElement = document.createElement('div')
+        rowElement.setAttribute('id', `row-${currentRowIndex}`)
+        console.debug(rowElement)
+        document.getElementById('tic-tac-toe-board').append(rowElement)
         var currentCellIndex = 0
         row.forEach( (cell) => {
             createCell(cell, currentCellIndex, `row-${currentRowIndex}`)
@@ -30,5 +43,5 @@ function refreshBoard(board) {
 
 function createCell(cell, cellIndex, rowId) {
     const cellElement = document.createElement('div').setAttribute('class', `cell-${cellIndex}`)
-    document.getElementById(rowId).appendChild(cellElement)
+    document.getElementById(rowId).append(cellElement)
 }

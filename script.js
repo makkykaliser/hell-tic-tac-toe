58765cc6f7e4f974d09fb8daf5e79875a5ffba4c
Makky Kaliser <49575758+makkykaliser@users.noreply.github.com>
2024-02-19 18:43:26 -0500

    working on initializing board, board is undefined

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
-console.debug("do we ball?")
+console.debug('intializing...')
+init()
 
-var board = [...Array(3)].map(e => Array(3)) // This makes an empty 3x3 2D array
+const Tile = { // Tile state enumeration
+    blank: "",
+    x: "x",
+    o: "o"
+}
+
+var board = [...Array(3)].map(e => Array(3)) // This makes an empty 3 x 3 2D array
+
+function init() {
+    console.debug(board)
+    refreshBoard(board)
+}
+
+function refreshBoard(board) {
+    var currentRowIndex = 0
+    board.forEach( (row) => {
+        const rowElement = document.createElement('div').setAttribute('id', `row-${currentRowIndex}`)
+        document.getElementById('tic-tac-toe-board').appendChild(rowElement)
+        var currentCellIndex = 0
+        row.forEach( (cell) => {
+            createCell(cell, currentCellIndex, `row-${currentRowIndex}`)
+            currentCellIndex++
+        })
+        currentRowIndex++
+    })
+}
+
+function createCell(cell, cellIndex, rowId) {
+    const cellElement = document.createElement('div').setAttribute('class', `cell-${cellIndex}`)
+    document.getElementById(rowId).appendChild(cellElement)
+}
